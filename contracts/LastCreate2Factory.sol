4901ca4130d3b727b4f5cc0496985819b9131558
0xTriple7 <ysqi1024@qq.com>
2022-07-21 16:50:33 +0800

    Simplify code and support callback

--- contracts/LastCreate2Factory.sol
+++ contracts/LastCreate2Factory.sol
@@ -1,11 +1,11 @@
 //SPDX-License-Identifier: MIT
-// Copy from:  https://raw.githubusercontent.com/0age/metamorphic/master/contracts/LastCreate2Factory.sol
+// Copy from: https://github.com/0age/metamorphic/blob/master/contracts/MetamorphicContractFactory.sol
 // Edited by devysq@gmail.com
-pragma solidity 0.8.15;
+pragma solidity 0.5.12;
 
 /**
  * @title Immutable Create2 Contract Factory
- * @author 0age
+ * @author 0age & ysqi
  * @notice This contract provides a safeCreate2 function that takes a salt value
  * and a block of initialization code as arguments and passes them into inline
  * assembly. The contract prevents redeploys by maintaining a mapping of all
@@ -23,7 +23,7 @@ contract LastCreate2Factory {
     event Deployed(address addr, bytes32 salt);
 
     // mapping to track which addresses have already been deployed.
-    mapping(address => bool) private _deployed;
+    mapping(address => bool) public deployed;
 
     /**
      * @notice create
@@ -34,49 +34,44 @@ contract LastCreate2Factory {
      * @param salt bytes32 The nonce that will be passed into the CREATE2 call.
      * @param initializationCode bytes The initialization code that will be passed
      * into the CREATE2 call.
+     * @param callData If callData is not empty, the new contract will be called
+     * with callData after the contract is created successfully.
+     * You can use it to initialize contracts, transfer management roles, or otherwise.
      * @return deploymentAddress is Address of the contract that will be created, or the null address
      * if a contract already exists at that address.
      */
-    function safeCreate2(bytes32 salt, bytes calldata initializationCode)
-        external
-        payable
-        containsCaller(salt)
-        returns (address deploymentAddress)
-    {
-        // move the initialization code from calldata to memory.
-        bytes memory initCode = initializationCode;
+    function safeCreate2(
+        bytes32 salt,
+        bytes calldata initializationCode,
+        bytes calldata callData
+    ) external returns (address deploymentAddress) {
+        // prevent contract submissions from being stolen from tx.pool by requiring
+        // that the first 20 bytes of the submitted salt match msg.sender.
+        require(
+            (bytes20(salt) == bytes20(0)) ||
+                (address(bytes20(salt)) == msg.sender),
+            "E1" // - first 20 bytes of the salt must match calling address.
+        );
 
         // determine the target address for contract deployment.
-        address targetDeploymentAddress = address(
-            uint160( // downcast to match the address type.
-                uint256( // convert to uint to truncate upper digits.
-                    keccak256( // compute the CREATE2 hash using 4 inputs.
-                        abi.encodePacked( // pack all inputs to the hash together.
-                            hex"ff", // start with 0xff to distinguish from RLP.
-                            address(this), // this contract will be the caller.
-                            salt, // pass in the supplied salt value.
-                            keccak256(abi.encodePacked(initCode)) // pass in the hash of initialization code.
-                        )
-                    )
-                )
-            )
+        address targetDeploymentAddress = _create2Address(
+            salt,
+            keccak256(abi.encodePacked(initializationCode))
         );
 
         // ensure that a contract hasn't been previously deployed to target address.
-        require(
-            !_deployed[targetDeploymentAddress],
-            "Invalid contract creation - contract has already been deployed."
-        );
+        require(!deployed[targetDeploymentAddress], "E2");
+
+        // move the initialization code from calldata to memory.
+        bytes memory initCode = initializationCode;
 
         // using inline assembly: load data and length of data, then call CREATE2.
         assembly {
             // solhint-disable-line
-            let encoded_data := add(0x20, initCode)
-            let encoded_size := mload(initCode)
             deploymentAddress := create2(
                 callvalue(), // forward any attached value.
-                encoded_data, // pass in initialization code.
-                encoded_size, // pass in init code's length.
+                add(0x20, initCode), // pass in initialization code.
+                mload(initCode), // pass in init code's length.
                 salt // pass in the salt value.
             )
             if iszero(extcodesize(deploymentAddress)) {
@@ -85,13 +80,16 @@ contract LastCreate2Factory {
         }
 
         // check address against target to ensure that deployment was successful.
-        require(
-            deploymentAddress == targetDeploymentAddress,
-            "Failed to deploy contract using provided salt and initialization code."
-        );
+        require(deploymentAddress == targetDeploymentAddress, "E3");
 
         // record the deployment of the contract to prevent redeploys.
-        _deployed[deploymentAddress] = true;
+        deployed[deploymentAddress] = true;
+
+        // call contract after created
+        if (callData.length > 0) {
+            (bool success, ) = deploymentAddress.call(callData);
+            require(success, "E4");
+        }
 
         emit Deployed(deploymentAddress, salt);
     }
@@ -116,98 +114,37 @@ contract LastCreate2Factory {
         returns (address deploymentAddress)
     {
         // determine the address where the contract will be deployed.
-        deploymentAddress = address(
-            uint160( // downcast to match the address type.
-                uint256( // convert to uint to truncate upper digits.
-                    keccak256( // compute the CREATE2 hash using 4 inputs.
-                        abi.encodePacked( // pack all inputs to the hash together.
-                            hex"ff", // start with 0xff to distinguish from RLP.
-                            address(this), // this contract will be the caller.
-                            salt, // pass in the supplied salt value.
-                            keccak256(abi.encodePacked(initCode)) // pass in the hash of initialization code.
-                        )
-                    )
-                )
-            )
+        deploymentAddress = _create2Address(
+            salt,
+            keccak256(abi.encodePacked(initCode))
         );
 
         // return null address to signify failure if contract has been deployed.
-        if (_deployed[deploymentAddress]) {
+        if (deployed[deploymentAddress]) {
             return address(0);
         }
     }
 
-    /**
-     * @dev Compute the address of the contract that will be created when
-     * submitting a given salt or nonce to the contract along with the keccak256
-     * hash of the contract's initialization code. The CREATE2 address is computed
-     * in accordance with EIP-1014, and adheres to the formula therein of
-     * `keccak256( 0xff ++ address ++ salt ++ keccak256(init_code)))[12:]` when
-     * performing the computation. The computed address is then checked for any
-     * existing contract code - if so, the null address will be returned instead.
-     * @param salt bytes32 The nonce passed into the CREATE2 address calculation.
-     * @param initCodeHash bytes32 The keccak256 hash of the initialization code
-     * that will be passed into the CREATE2 address calculation.
-     * @return deploymentAddress is Address of the contract that will be created, or the null address
-     * if a contract has already been deployed to that address.
-     */
-    function findCreate2AddressViaHash(bytes32 salt, bytes32 initCodeHash)
-        external
+    // determine the target address for contract deployment.
+    function _create2Address(bytes32 salt, bytes32 initCodeHash)
+        private
         view
-        returns (address deploymentAddress)
+        returns (address)
     {
-        // determine the address where the contract will be deployed.
-        deploymentAddress = address(
-            uint160( // downcast to match the address type.
-                uint256( // convert to uint to truncate upper digits.
-                    keccak256( // compute the CREATE2 hash using 4 inputs.
-                        abi.encodePacked( // pack all inputs to the hash together.
-                            hex"ff", // start with 0xff to distinguish from RLP.
-                            address(this), // this contract will be the caller.
-                            salt, // pass in the supplied salt value.
-                            initCodeHash // pass in the hash of initialization code.
+        return
+            address(
+                uint160( // downcast to match the address type.
+                    uint256( // convert to uint to truncate upper digits.
+                        keccak256( // compute the CREATE2 hash using 4 inputs.
+                            abi.encodePacked( // pack all inputs to the hash together.
+                                hex"ff", // start with 0xff to distinguish from RLP.
+                                address(this), // this contract will be the caller.
+                                salt, // pass in the supplied salt value.
+                                initCodeHash // pass in the hash of initialization code.
+                            )
                         )
                     )
                 )
-            )
-        );
-
-        // return null address to signify failure if contract has been deployed.
-        if (_deployed[deploymentAddress]) {
-            return address(0);
-        }
-    }
-
-    /**
-     * @dev Determine if a contract has already been deployed by the factory to a
-     * given address.
-     * @param deploymentAddress address The contract address to check.
-     * @return True if the contract has been deployed, false otherwise.
-     */
-    function hasBeenDeployed(address deploymentAddress)
-        external
-        view
-        returns (bool)
-    {
-        // determine if a contract has been deployed to the provided address.
-        return _deployed[deploymentAddress];
-    }
-
-    /**
-     * @dev Modifier to ensure that the first 20 bytes of a submitted salt match
-     * those of the calling account. This provides protection against the salt
-     * being stolen by frontrunners or other attackers. The protection can also be
-     * bypassed if desired by setting each of the first 20 bytes to zero.
-     * @param salt bytes32 The salt value to check against the calling address.
-     */
-    modifier containsCaller(bytes32 salt) {
-        // prevent contract submissions from being stolen from tx.pool by requiring
-        // that the first 20 bytes of the submitted salt match msg.sender.
-        require(
-            (address(bytes20(salt)) == msg.sender) ||
-                (bytes20(salt) == bytes20(0)),
-            "Invalid salt - first 20 bytes of the salt must match calling address."
-        );
-        _;
+            );
     }
 }
